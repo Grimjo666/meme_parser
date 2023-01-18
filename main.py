31b92aa2629b76fdda0ce34f619f4ccc006beec6
Grimjo666 <120495866+Grimjo666@users.noreply.github.com>
2023-01-18 17:35:06 +0300

    add some rows in main

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ def get_links_on_page_memes():
     response = requests.get("https://memepedia.ru/memoteka/", headers=headers)
     soup = BeautifulSoup(response.text, "lxml")
 
-    list_memes = soup.find_all('li', class_='post-item post-item-masonry-boxed')
+    list_memes = soup.find_all("li", class_="post-item post-item-masonry-boxed")
 
     for item in list_memes:
-        url = item.find('a').get('href')
+        url = item.find("a").get("href")
         yield url
         sleep(0.3)
 
+
+for link in get_links_on_page_memes():
+    res_page = requests.get(link, headers=headers)
+    soup_page = BeautifulSoup(res_page.text, "lxml")
+
+    meme_name = soup_page.find("h1", class_="entry-title s-post-title bb-mb-el")
+    meme_pic = soup_page.find("figure", class_="post-thumbnail").find("img").get("src")
+    description = soup_page.find("div", class_="js-mediator-article").find("p").text
+    origin = ''.join([p.text for p in soup_page.find_all("h2")[1].find_all_next("p")[:3]])
